f756c443970395f6d3330167c71c2d220b63255f
Nick van den Dungen <nicktvdd@hotmail.com>
2024-02-22 17:37:16 +0200

    init py and extra test

--- studentapp/tests/test_main.py
+++ studentapp/tests/test_main.py
@@ -21,6 +21,12 @@ def test_create_student():
     assert response.status_code == 200
     assert response.json() == student.model_dump()
 
+def test_create_student():
+    student = Student(id=1, name="John", age=17, class_="year 12")
+    response = client.post("/students/1", json=student.model_dump())
+    assert response.status_code == 200
+    assert response.json() == student.model_dump()
+
 def test_get_student():
     response = client.get("/students/1")
     assert response.status_code == 200
@@ -92,7 +98,6 @@ def test_get_student_by_name_multiple_students_same_name():
     student2 = Student(id=3, name="John", age=18, class_="year 13")
     response1 = client.post("/students/2", json=student1.model_dump())
     response2 = client.post("/students/3", json=student2.model_dump())
-    
     assert response1.status_code == 200
     assert response1.json() == student1.model_dump()
     assert response2.status_code == 200
@@ -103,11 +108,14 @@ def test_get_student_by_name_multiple_students_same_name():
     assert len(response.json()) == 2
 
 def test_get_all_students_order():
-    student1 = Student(id=1, name="John", age=17, class_="year 12")
-    student2 = Student(id=2, name="Jane", age=18, class_="year 13")
-    client.post("/students/1", json=student1.model_dump())
-    client.post("/students/2", json=student2.model_dump())
+    student1 = Student(id=4, name="John", age=17, class_="year 12")
+    student2 = Student(id=5, name="Jane", age=18, class_="year 13")
+    client.post("/students/4", json=student1.model_dump())
+    client.post("/students/5", json=student2.model_dump())
     response = client.get("/students")
     assert response.status_code == 200
     assert response.json()[0]["id"] == 1
     assert response.json()[1]["id"] == 2
+    assert response.json()[2]["id"] == 3
+    assert response.json()[3]["id"] == 4
+    assert response.json()[4]["id"] == 5
